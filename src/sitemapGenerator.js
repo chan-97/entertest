70b90281a546b9de124e2d3dde05ec9d53756290
HwiChanYu <dbgnlcks34@gmail.com>
2023-03-26 18:48:28 +0900

    chore: Generate sitemap before build

---
+++ src/sitemapGenerator.js
@@ -0,0 +1,15 @@
+import fs from "fs"
+
+const generateSitemap = () => {
+  const date = new Date().toISOString().slice(0, 10);
+  
+  return `
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://idoltest.com/</loc>
+    <lastmod>${date}</lastmod>
+  </url>
+</urlset>`
+}
+
+fs.writeFileSync('public/sitemap.xml', `<?xml version="1.0" encoding="UTF-8"?>${generateSitemap()}`);
